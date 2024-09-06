7c17aa401c63834cb63ee759d732b4f7e2ddd66e
Tedy Tri Saputro <tedy@saputro.dev>
2024-09-06 07:35:40 +0700

    add application-scoped

---
+++ 06-quarkus-core/application-scoped/first-quarkus-project/src/main/java/com/subrutin/quarkus/FirstQuarkusProjectApp.java
@@ -0,0 +1,11 @@
+package com.subrutin.quarkus;
+
+import io.quarkus.runtime.Quarkus;
+
+public class FirstQuarkusProjectApp {
+	
+	public static void main(String[] args) {
+		Quarkus.run(args);
+	}
+
+}
---
+++ 06-quarkus-core/application-scoped/first-quarkus-project/src/main/java/com/subrutin/quarkus/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.subrutin.quarkus.config;
+
+import com.subrutin.quarkus.domain.Author;
+
+import jakarta.enterprise.context.Dependent;
+import jakarta.enterprise.inject.Produces;
+
+@Dependent
+public class AppConfig {
+
+    @Produces
+    public  Author author() {
+        Author author = new Author();
+        author.setName("Karen Armstrong");
+        return author;
+    }
+
+}
